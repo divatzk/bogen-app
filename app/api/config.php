75ab253492c007514a049a119d583a9d63b6eeea
David Leeb <dave.leeb@gmail.com>
2015-11-28 16:38:35 +0100

    init

--- app/api/config.php
+++ app/api/config.php
@@ -8,14 +8,3 @@ define(DB_PROD_USER, "TODO");
 define(DB_PROD_PASSWORD, "TODO");
 define(DB_PROD_DATABASE, "TODO");
 define(DB_PROD_HOST, "TODO");
-
-function getDBConnection() {
-	$isdevelopment = strpos($_SERVER['HTTP_HOST'], DB_LOCAL_HOST) !== false;
-	$dbhost = $isdevelopment ? DB_LOAL_HOST : DB_PROD_HOST;
-	$dbuser = $isdevelopment ? DB_LOCAL_USER : DB_PROD_HOST;
-	$dbpass = $isdevelopment ? DB_LOCAL_PASSWORD : DB_PROD_PASSWORD;
-	$dbname = $isdevelopment ? DB_LOCAL_DATABASE : DB_PROD_DATABASE;
-	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return $dbh;
-}
--- app/api/index.php
+++ app/api/index.php
@@ -57,3 +57,14 @@ function addGame() {
 			echo '{"error":{"text":'. $e->getMessage() .'}}';
 	}
 }
+
+function getDBConnection() {
+	$isdevelopment = strpos($_SERVER['HTTP_HOST'], DB_LOCAL_HOST) !== false;
+	$dbhost = $isdevelopment ? DB_LOAL_HOST : DB_PROD_HOST;
+	$dbuser = $isdevelopment ? DB_LOCAL_USER : DB_PROD_HOST;
+	$dbpass = $isdevelopment ? DB_LOCAL_PASSWORD : DB_PROD_PASSWORD;
+	$dbname = $isdevelopment ? DB_LOCAL_DATABASE : DB_PROD_DATABASE;
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbh;
+}
